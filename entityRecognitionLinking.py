eed1eb144a363d84a095578bbe0c4c800060369f
Vinod Gokul Pillai <48096481+VGokulPillai@users.noreply.github.com>
2022-08-06 00:38:38 +0100

    Update entityRecognitionLinking.py

--- entityRecognitionLinking.py
+++ entityRecognitionLinking.py
@@ -1,6 +1,6 @@
 # Takes entity/relation as input and links it to DBpedia.
 # Need to add additional code to rank the DBpedia results based on context and choose the best matching entity.
-# Reference: https://medium.com/analytics-vidhya/entity-linking-a-primary-nlp-task-for-information-extraction-22f9d4b90aa8
+
 
 import requests
 import json
@@ -23,7 +23,6 @@ class EntityRecognitionLinking:
         # Parameters
         # 'text' - text to be annotated
         # 'confidence' -   confidence score for linking
-        #params = {"text": "My name is Sundar. I am currently doing Master's in Artificial Intelligence at NUS. I love Natural Language Processing.", "confidence": 0.35}
         params = {"text": text, "confidence": 0.1}
         # Response content type
         #headers = {'accept': 'text/html'}
@@ -33,8 +32,5 @@ class EntityRecognitionLinking:
         if res.status_code != 200:
             # Something went wrong
             raise APIError(res.status_code)
-        # Display the result as HTML in Jupyter Notebook
-        # display(HTML(res.text))
-        # Pretty printing as json
         print(json.dumps(json.loads(res.text), sort_keys=True, indent=4))
         return json.loads(res.text)
